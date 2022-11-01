736a8e9dbb3e9d1d5decfeafdffbd68a2f5ca413
Eckhoff42 <magnus.wiik.eckhoff@gmail.com>
2022-11-01 08:37:41 +0100

    encode not done

--- Compressor.py
+++ Compressor.py
@@ -26,3 +26,25 @@ class Compressor():
                 new_string += " " + term
 
         return new_string
+
+    def encode_variable_bit(self, number: int) -> str:
+        """
+        Encodes a number using variable bit encoding
+        """
+        # binary datatype
+        binary = bin(number)[2:]
+
+        # round base up to nearest integer
+        base = len(binary)/7
+        base = int(base) + 1 if base % 1 != 0 else int(base)
+
+        res = ""
+        for i in range(base):
+            pass
+
+        rest = binary[i*7:]
+        # add 7 - len(rest) "0" to res
+        res += "0" * (7 - len(rest))
+        res += rest
+
+        return res
